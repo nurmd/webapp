d1d7372a753fc726e612a9aa20279fc55d9dfbee
Ain <nurmd111@gmail.com>
2020-05-08 21:51:33 +0530

    Update blockstack.ts

--- src/constants/blockstack.ts
+++ src/constants/blockstack.ts
@@ -3,6 +3,6 @@ import routes from 'utils/routes'
 
 export const appConfig = new AppConfig(
   ['store_write', 'publish_data'],
-  'https://pixus.local:3000',
+  'https://pixhub.local:3000',
   routes.authVerify,
 )
